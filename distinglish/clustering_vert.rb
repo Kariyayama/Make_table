aa51cc54fb4ee2404e403a755ae3080fde006ac0
Kariyayama <39517197+Kariyayama@users.noreply.github.com>
2018-07-03 20:23:01 +0900

    Add file to distinglish each class

---
+++ distinglish/clustering_vert.rb
@@ -0,0 +1,25 @@
+domfile    = ARGV.shift
+domcomfile = ARGV.shift
+
+phash = Hash.new
+file = File.open(domfile, "r")
+file.each_line do |line1|
+  l1 = line1.split(',')
+  pf = l1[0]
+  if l1[1..8] == Array.new(8){"0"} then
+    phash.store(l1[0], 0)
+  end
+end
+file.close  
+
+
+file2 = File.open(domcomfile, "r")
+file2.each_line do |linec|
+  i = 0
+  lc = linec.chomp.split(',')
+  if phash.fetch(lc[-2],nil) == nil && phash.fetch(lc[-1], nil) == nil  then  puts "#{linec.chomp},class2"
+  elsif phash.fetch(lc[-2],nil) == nil || phash.fetch(lc[-1], nil) == nil  then  puts "#{linec.chomp},class1"
+  else puts "#{linec.chomp},uniq"
+  end
+end
+file2.close
